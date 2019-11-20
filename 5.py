57f172f2c8612f5de95e006f2c523250986a472e
vineethsudhir <vineeth.sudhir@gmail.com>
2019-11-20 07:06:16 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,99 +1,79 @@
-import csv
-import random
-import math
-def loadCsv(filename):
-    lines=csv.reader(open(filename,"r"))
-    dataset=list(lines)
-    for i in range(len(dataset)):
-        dataset[i]=[float(x) for x in dataset[i]]
-    return dataset
+import pandas as pd
+import numpy as np
 
-def splitDataset(dataset,splitRatio):
-    trainSize=int(len(dataset)*splitRatio)
-    trainSet=[]
-    copy=list(dataset)
-    while len(trainSet)<trainSize:
-        index=random.randrange(len(copy))
-        trainSet.append(copy.pop(index))
-    return [trainSet,copy]
+dataset = pd.read_csv('pima-indians-diabetes.data.csv')
 
-def separateByClass(dataset):
-    separated={}
-    for i in range(len(dataset)):
-        vector=dataset[i]
-        if(vector[-1] not in separated):
-            separated[vector[-1]]=[]
-        separated[vector[-1]].append(vector)
-        
-    return separated
+dataset.columns = [
+    "NumTimesPrg", "PlGlcConc", "BloodP",
+    "SkinThick", "TwoHourSerIns", "BMI",
+    "DiPedFunc", "Age", "HasDiabetes"]
+
+dataset.shape
+
+#1st output so press enter
+
+corr = dataset.corr()
 
 
-def mean(numbers):
-    return sum(numbers)/float(len(numbers))
 
-def stdev(numbers):
-    avg=mean(numbers)
-    variance=sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
-    return math.sqrt(variance)
+median_bmi = dataset['BMI'].median()
+dataset['BMI'] = dataset['BMI'].replace(
+    to_replace=0, value=median_bmi)
 
-def summarize(dataset):
-    summaries=[(mean(attribute),stdev(attribute)) for attribute in zip(*dataset)]
-    del summaries[-1]
-    return summaries
 
-def summarizeByClass(dataset):
-    separated=separateByClass(dataset)
-    summaries={}
-    for classValue,instances in separated.items():
-        summaries[classValue]=summarize(instances)
-    return summaries
+median_bloodp = dataset['BloodP'].median()
 
-def calculateProbability(x,mean,stdev):
-    exponent=math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
-    return (1/(math.sqrt(2*math.pi)*stdev))*exponent
+dataset['BloodP'] = dataset['BloodP'].replace(
+    to_replace=0, value=median_bloodp)
 
-def calculateClassProbabilities(summaries,inputVector):
-    probabilities={}
-    for classValue,classSummaries in summaries.items():
-        probabilities[classValue]=1
-        for i in range(len(classSummaries)):
-            mean,stdev=classSummaries[i]
-            x=inputVector[i]
-            probabilities[classValue]*=calculateProbability(x,mean,stdev)
-    return probabilities
 
-def predict(summaries,inputVector):
-    probabilities=calculateClassProbabilities(summaries,inputVector)
-    bestLabel,bestProb=None,-1
-    for classValue,probability in probabilities.items():
-        if bestLabel is None or probability>bestProb:
-            bestProb=probability
-            bestLabel=classValue
-    return bestLabel
+median_plglcconc = dataset['PlGlcConc'].median()
 
-def getPredictions(summaries,testSet):
-    predictions=[]
-    for i in range(len(testSet)):
-        result=predict(summaries,testSet[i])
-        predictions.append(result)
-    return predictions
+dataset['PlGlcConc'] = dataset['PlGlcConc'].replace(
+    to_replace=0, value=median_plglcconc)
 
-def getAccuracy(testSet,predictions):
-    correct=0
-    for i in range(len(testSet)):
-        if testSet[i][-1]==predictions[i]:
-            correct+=1
-        return (correct/float(len(testSet)))*100.0
+median_skinthick = dataset['SkinThick'].median()
+dataset['SkinThick'] = dataset['SkinThick'].replace(
+    to_replace=0, value=median_skinthick)
+
+
+median_twohourserins = dataset['TwoHourSerIns'].median()
+
+dataset['TwoHourSerIns'] = dataset['TwoHourSerIns'].replace(
+    to_replace=0, value=median_twohourserins)
+
+from sklearn.model_selection import train_test_split
+train_set, test_set = train_test_split(
+    dataset, test_size=0.2, random_state=42)
+
+
+train_set_labels = train_set["HasDiabetes"].copy()
+train_set = train_set.drop("HasDiabetes", axis=1)
+
+test_set_labels = test_set["HasDiabetes"].copy()
+test_set = test_set.drop("HasDiabetes", axis=1)
+
+
+from sklearn.preprocessing import MinMaxScaler as Scaler
+
+scaler = Scaler()
+scaler.fit(train_set)
+train_set_scaled = scaler.transform(train_set)
+test_set_scaled = scaler.transform(test_set)
+
+from sklearn.naive_bayes import GaussianNB
+
+X = train_set_scaled
+Y = train_set_labels
+
+clf =GaussianNB()
+
     
-def main():
-    filename='pima-indians-diabetes.data.csv'
-    splitRatio=0.67
-    dataset=loadCsv(filename)
-    trainingSet,testSet=splitDataset(dataset,splitRatio)
-    print('Split {0} rows into train={1} and test={2} rows'.format(len(dataset),len(trainingSet),len(testSet)))
-    summaries=summarizeByClass(trainingSet)
-    predictions=getPredictions(summaries,testSet)
-    accuracy=getAccuracy(testSet,predictions)
-    print('Accuracy:{0}%'.format(accuracy))
-    
-main()
+clf.fit(X,Y)
+
+pred = clf.predict(test_set) 
+
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(pred,test_set_labels) 
+print( accuracy)
