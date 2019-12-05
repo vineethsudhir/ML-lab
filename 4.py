86b2ea6084a21513d5655e44cbfe562505a0effd
vineethsudhir <vineeth.sudhir@gmail.com>
2019-12-05 15:30:01 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,12 +1,16 @@
 import numpy as np
+
 X=np.array(([2,9],[1,5],[3,6]),dtype=float)
 y=np.array(([92],[86],[89]),dtype=float)
 X=X/np.amax(X,axis=0)
 y=y/100
+
 def sigmoid(x):
     return 1/(1+np.exp(-x))
+
 def derivatives_sigmoid(x):
     return x*(1-x)
+
 epoch=7000
 lr=0.1
 inputlayer_neurons=2
@@ -36,4 +40,3 @@ for i in range(epoch):
 print("Input:\n"+str(X))
 print("Actual Output:\n"+str(y))
 print("Predicted Output:\n",output)
- 
